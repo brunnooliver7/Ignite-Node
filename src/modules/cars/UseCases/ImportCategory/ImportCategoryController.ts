dbc61466d5f9dc0f0c2aabe57dded3643de84406
Bruno Oliveira <brunno.oliver7@gmail.com>
2021-10-25 12:10:24 -0300

    2.5.3

---
+++ src/modules/cars/UseCases/ImportCategory/ImportCategoryController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+class ImportCategoryController {
+  constructor(private importcategoryusecase: ImportCategoryUseCase) {}
+
+  handle(req: Request, res: Response): Response {
+    const { file } = req;
+    this.importcategoryusecase.execute(file);
+    return res.send();
+  }
+}
+
+export { ImportCategoryController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/UseCases/CreateCategory";
+import { importCategoryController } from "../modules/cars/UseCases/ImportCategory";
 import { listCategoriesController } from "../modules/cars/UseCases/ListCategories";
 
 const categoriesRoutes = Router();
@@ -19,9 +20,7 @@ categoriesRoutes.get("/", (req, res) => {
 });
 
 categoriesRoutes.post("/import", updload.single("file"), (req, res) => {
-  const { file } = req;
-  console.log(file);
-  return res.send();
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
