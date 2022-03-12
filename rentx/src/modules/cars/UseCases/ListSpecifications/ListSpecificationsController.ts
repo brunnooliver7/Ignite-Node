afb481c26bd35467929fbe154d5150a0aed9b9a2
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-03-12 19:48:24 -0300

    correcoes

--- rentx/src/modules/cars/UseCases/ListSpecifications/ListSpecificationsController.ts
+++ rentx/src/modules/cars/UseCases/ListSpecifications/ListSpecificationsController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
-
 import { ListSpecificationsUseCase } from "./ListSpecificationsUseCase";
 
 class ListSpecificationsController {
