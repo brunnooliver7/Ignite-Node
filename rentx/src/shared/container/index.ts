9d0fc0d68abd091dcc2e0eee69365f266dda14be
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-03-22 21:10:01 -0300

    create car

--- rentx/src/shared/container/index.ts
+++ rentx/src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { container } from 'tsyringe';
@@ -19,4 +21,9 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UserRepository
+);
+
+container.registerSingleton<ICarsRepository>(
+  "CarsRepository",
+  CarsRepository
 );
