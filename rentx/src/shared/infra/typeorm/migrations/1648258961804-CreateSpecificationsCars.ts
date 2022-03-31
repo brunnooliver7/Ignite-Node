8a27051f830d47764cbcd001da7d8acc780c7c1a
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-03-30 23:18:35 -0300

    ajustes migrations

--- rentx/src/shared/infra/typeorm/migrations/1648258961804-CreateSpecificationsCars.ts
+++ rentx/src/shared/infra/typeorm/migrations/1648258961804-CreateSpecificationsCars.ts
@@ -9,7 +9,8 @@ export class CreateSpecificationsCars1648258961804 implements MigrationInterface
         columns: [
           {
             name: "car_id",
-            type: "uuid"
+            type: "uuid",
+            isPrimary: true
           },
           {
             name: "specification_id",
