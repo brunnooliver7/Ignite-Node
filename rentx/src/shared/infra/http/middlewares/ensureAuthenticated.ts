5d65cdbb42e28799ce71bd5311ca720f2190d6f2
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-04-21 21:26:55 -0300

    corrigindo refresh token

--- rentx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ rentx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,6 +1,4 @@
 import auth from "@config/auth";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
-import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
@@ -12,8 +10,6 @@ interface IPayload {
 export async function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization;
 
-  const usersTokensRepository = new UsersTokensRepository();
-
   if (!authHeader) {
     throw new AppError("Token missing", 401);
   }
@@ -23,17 +19,8 @@ export async function ensureAuthenticated(req: Request, res: Response, next: Nex
   try {
     const { sub: user_id } = verify(
       token,
-      auth.secret_refresh_token
+      auth.secret_token
     ) as IPayload;
-
-    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token
-    );
-
-    if (!user) {
-      throw new AppError("User does not exists", 401);
-    }
 
     req.user = {
       id: user_id
