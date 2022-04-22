271acd9eba0201d17be5fb3e5dc59f9715fdab5c
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-04-22 17:30:12 -0300

    criando url de acesso do avatar

--- rentx/src/modules/cars/UseCases/uploadCarImage/UploadCarImagesUseCase.ts
+++ rentx/src/modules/cars/UseCases/uploadCarImage/UploadCarImagesUseCase.ts
@@ -1,4 +1,7 @@
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { ICarsImagesRespository } from "@modules/cars/repositories/ICarsImagesRepositoy";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
+import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
 
 interface IRequest {
@@ -8,15 +11,27 @@ interface IRequest {
 
 @injectable()
 class UploadCarImagesUseCase {
-
   constructor(
     @inject("CarsImagesRepository")
-    private carImageRepository: ICarsImagesRespository
+    private carImageRepository: ICarsImagesRespository,
+
+    // @inject("CarsRepository")
+    // private carsRepository: CarsRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) { }
 
   async execute({ car_id, images_name }: IRequest) {
+    // const car = await this.carsRepository.findById(car_id);
+
+    // if (!car) {
+    //   throw new AppError("Car does not exists");
+    // }
+
     images_name.map(async image => {
       await this.carImageRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
--- rentx/src/shared/infra/http/app.ts
+++ rentx/src/shared/infra/http/app.ts
@@ -7,6 +7,7 @@ import "reflect-metadata";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
+import upload from "@config/upload";
 
 createConnection();
 
@@ -14,7 +15,12 @@ const app = express();
 
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
+
 app.use(router);
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
